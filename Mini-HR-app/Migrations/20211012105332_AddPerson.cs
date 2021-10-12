84a3a934f879b9922abdfb298c8be8d1f8cc2e38
Amalia Vargatu <avargatu@yahoo.com>
2021-10-12 20:03:04 +0300

    add company, employee and person models, add company and employees controllers and view models

---
+++ Mini-HR-app/Migrations/20211012105332_AddPerson.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mini_HR_app.Migrations
+{
+    public partial class AddPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmploymentDate",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PersonId",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ssn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BirthPlace = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_PersonId",
+                table: "Employee",
+                column: "PersonId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Person_PersonId",
+                table: "Employee",
+                column: "PersonId",
+                principalTable: "Person",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Person_PersonId",
+                table: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_PersonId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                table: "Employee");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmploymentDate",
+                table: "Employee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ Mini-HR-app/ViewModels/PersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mini_HR_app.ViewModels
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Ssn { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+    }
+}
